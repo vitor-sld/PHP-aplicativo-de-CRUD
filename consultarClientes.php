b8cd3dcadbc66089d7b868177bd40459f6d874eb
vitor-sld <48792332+vitor-sld@users.noreply.github.com>
2021-10-30 23:00:08 -0300

    App Formulário V1
    Formulário com função CRUD

---
+++ consultarClientes.php
@@ -0,0 +1,100 @@
+<?php
+require_once("controller/ControllerCadastro.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="format-detection" content="telephone=no"> <meta name="msapplication-tap-highlight" content="no">
+	<meta name="viewport" content="initial-scale=1, width=device-width, viewport-fit=cover"> <meta name="color-scheme" content="light dark"> 
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.css"> 
+	<link rel="stylesheet" href="css/estilo.css">
+	<script src="js/jquery.js"></script>
+	<script src="bootstrap/js/bootstrap.js"></script>
+	<!--<script src="js/funcoes.js"></script>-->
+	<script>
+		function confirmDelete(delUrl) {
+  			if (confirm("Deseja apagar o registro?")) {
+   				document.location = delUrl;
+   				var url_string = "http://localhost/agendamentos-mysql/" + delUrl;
+				var url = new URL(url_string);
+				var data = url.searchParams.get("id"); //pega o value
+	        }  
+		}
+	</script>
+	<title>SISTEMA DE AGENDAMENTO - CLIENTES</title>
+</head> 
+<body> 
+	<div class="container">
+		<div class="row">
+			<nav class="navbar navbar-expand-lg navbar-dark bg-primary col-12">
+				<a class="navbar-brand" href="#">SISTEMA WEB</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarNavDropdown">
+					<ul class="navbar-nav">
+						<li class="nav-item active">
+							<a class="nav-link" href="index.php">Cadastrar<span class="sr-only">(current)</span></a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="consultarClientes.php">Consultar</a>
+						</li>
+					</ul>
+				</div>
+			</nav>  
+		</div>
+		<div class="row">
+			<div class="card mb-3 col-12">
+				<div class="card-body" style="margin: auto;">
+					<h5 class="card-title">Consultar - Contatos Agendados</h5>
+					<table class="table table-responsive" style="width: auto;">
+						<thead class="table-active bg-primary">
+							<tr>
+								<th scope="col">Nome</th>
+								<th scope="col">Telefone</th>
+								<th scope="col">Origem</th>
+								<th scope="col">Contato</th>
+								<th scope="col">Observação</th>
+								<th scope="col">Ação</th>
+							</tr>
+						</thead>
+						<tbody id="TableData">
+						<?php
+							$controller = new ControllerCadastro();
+							$resultado = $controller->listar(0);
+							//print_r($resultado);
+							for($i=0;$i<count($resultado);$i++){ 
+						?>
+								<tr>
+									<td scope="col"><?php echo $resultado[$i]['nome']; ?></td>
+									<td scope="col"><?php echo $resultado[$i]['telefone']; ?></td>
+									<td scope="col"><?php echo $resultado[$i]['origem']; ?></td>
+									<td scope="col"><?php echo $resultado[$i]['data_contato']; ?></td>
+									<td scope="col"><?php echo $resultado[$i]['observacao']; ?></td>
+									<td scope="col">
+										<button type="button" class="btn btn-outline-primary" onclick="location.href='editarClientes.php?id=<?php echo $resultado[$i]['id']; ?>'" style="width: 72px;">Editar</button>
+										<button type="button" class="btn btn-outline-primary" onclick="javascript:confirmDelete('excluirClientes.php?id=<?php echo $resultado[$i]['id']; ?>')" style="width: 72px;">Excluir</button>
+									</td>
+								</tr>
+						<?php
+							}
+						?>
+						</tbody>
+						<!--<tr>
+							<td scope="row">Jeferson Roberto de Lima</td>
+							<td>(11)97665-0099</td>
+							<td>Google Meu Negócio</td>
+							<td>25/05/2021</td>
+							<td>Serviço agendado para 01/07/2021</td>
+							<td>
+								<button type="button" class="btn btn-outline-primary" style="width: 72px;">Editar</button>
+								<button type="button" class="btn btn-outline-primary" style="width: 72px;">Excluir</button>
+							</td>
+						</tr>-->
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
---
+++ model/banco.php
@@ -0,0 +1,69 @@
+<?php
+//timezone
+date_default_timezone_set('America/Sao_Paulo');
+
+// conexão com o banco de dados
+define('BD_SERVIDOR','localhost');
+define('BD_USUARIO','root');
+define('BD_SENHA','');
+define('BD_BANCO','agendamentos');
+    
+class Banco{
+
+    protected $mysqli;
+    private $cadastro;
+
+    public function __construct(){
+        $this->conexao();
+    }
+
+    private function conexao(){
+        $this->mysqli = new mysqli(BD_SERVIDOR, BD_USUARIO , BD_SENHA, BD_BANCO);
+    }
+
+    public function setAgendamentos($nome,$telefone,$origem,$data_contato,$observacao){
+        $stmt = $this->mysqli->prepare("INSERT INTO agendamento (`nome`, `telefone`, `origem`, `data_contato`, `observacao`) VALUES (?,?,?,?,?);");
+        $stmt->bind_param("sssss",$nome,$telefone,$origem,$data_contato,$observacao);
+        if( $stmt->execute() == TRUE){
+            return true ;
+        }else{
+            return false;
+        }
+    }
+
+    public function getAgendamentos($id){
+        try {
+            if(isset($id) && $id > 0){
+                $stmt = $this->mysqli->query("SELECT * FROM agendamento WHERE id = '" . $id . "';");
+            }else{
+                $stmt = $this->mysqli->query("SELECT * FROM agendamento;");
+            }
+            
+            $lista = $stmt->fetch_all(MYSQLI_ASSOC);
+            $f_lista = array();
+            $i = 0;
+            foreach ($lista as $l) {
+                $f_lista[$i]['id'] = $l['id'];
+                $f_lista[$i]['nome'] = $l['nome'];
+                $f_lista[$i]['telefone'] = $l['telefone'];
+                $f_lista[$i]['origem'] = $l['origem'];
+                $f_lista[$i]['data_contato'] = $l['data_contato'];
+                $f_lista[$i]['observacao'] = $l['observacao'];
+                $i++;
+            }
+            return $f_lista;
+        } catch (Exception $e) {
+            echo "Ocorreu um erro ao tentar Buscar Todos." . $e;
+        }
+    }
+
+    public function updateAgendamentos($id,$nome,$telefone,$origem,$data_contato,$observacao){
+       $stmt = $this->mysqli->query("UPDATE agendamentos SET `nome` = '" . $nome . "', `telefone` =  '" . $telefone . "', `origem` =  '" . $origem . "', `data_contato` =  '" . $data_contato . "', `observacao` =   '" . $observacao . "' WHERE `id` =  '" . $id . "';");
+        if( $stmt > 0){
+            return true ;
+        }else{
+            return false;
+        }
+    }
+}    
+?>
